e19731545e030196a1eecf648815237ddac3e3e3
silasETH <silstocker@student.ethz.ch>
2023-03-23 00:52:29 +0100

    sperrgut_stgallen_scanner init

---
+++ sperrgut_stgallen_scanner/android/app/src/main/kotlin/com/example/sperrgut_stgallen_scanner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sperrgut_stgallen_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sperrgut_stgallen_scanner/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sperrgut_stgallen_scanner/app/app.bottomsheets.dart';
+import 'package:sperrgut_stgallen_scanner/app/app.dialogs.dart';
+import 'package:sperrgut_stgallen_scanner/app/app.locator.dart';
+import 'package:sperrgut_stgallen_scanner/app/app.router.dart';
+import 'package:sperrgut_stgallen_scanner/ui/common/app_colors.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+void main() {
+  setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: Theme.of(context).copyWith(
+        primaryColor: kcBackgroundColor,
+        focusColor: kcPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+            ),
+      ),
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [
+        StackedService.routeObserver,
+      ],
+    );
+  }
+}
