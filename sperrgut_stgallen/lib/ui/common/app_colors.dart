6703a6cb69b2cc4df6715eed462071b460021929
XYQuadrat <mail@xyquadrat.ch>
2023-03-23 01:31:38 +0100

    Add official St. Gallen colors
    Taken from https://company-41578.frontify.com/d/xD0Idv8S6VM3/basiselemente#/farben/markenfarben

--- sperrgut_stgallen/lib/ui/common/app_colors.dart
+++ sperrgut_stgallen/lib/ui/common/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const Color kcPrimaryColor = Color(0xFF9600FF);
-const Color kcPrimaryColorDark = Color(0xFF300151);
+const Color kcPrimaryColor = Color(0xFFE00025);
+const Color kcPrimaryColorDark = Color(0xFF84171C);
 const Color kcDarkGreyColor = Color(0xFF1A1B1E);
-const Color kcMediumGrey = Color(0xFF474A54);
-const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
-const Color kcVeryLightGrey = Color(0xFFE3E3E3);
+const Color kcMediumGrey = Color(0xFFA7A7A7);
+const Color kcLightGrey = Color(0xFFBFBFBF);
+const Color kcVeryLightGrey = Color(0xFFDDDDDD);
 const Color kcBackgroundColor = kcDarkGreyColor;
