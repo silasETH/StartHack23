b1c04534c91071266bc31e104a21a2f3fc45de3f
XYQuadrat <mail@xyquadrat.ch>
2023-03-23 12:46:00 +0100

    Remove startup screen visuals & wait

--- sperrgut_stgallen/lib/ui/views/startup/startup_view.dart
+++ sperrgut_stgallen/lib/ui/views/startup/startup_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
-import 'package:sperrgut_stgallen/ui/common/ui_helpers.dart';
 
 import 'startup_viewmodel.dart';
 
@@ -18,35 +17,7 @@ class StartupView extends StackedView<StartupViewModel> {
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'STACKED',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                Text(
-                  'Loading ...',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                )
-              ],
-            ),
-          ],
+          children: const [],
         ),
       ),
     );
--- sperrgut_stgallen/lib/ui/views/startup/startup_viewmodel.dart
+++ sperrgut_stgallen/lib/ui/views/startup/startup_viewmodel.dart
@@ -1,6 +1,6 @@
-import 'package:stacked/stacked.dart';
 import 'package:sperrgut_stgallen/app/app.locator.dart';
 import 'package:sperrgut_stgallen/app/app.router.dart';
+import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
@@ -8,8 +8,6 @@ class StartupViewModel extends BaseViewModel {
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
-    await Future.delayed(const Duration(seconds: 3));
-
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
 
