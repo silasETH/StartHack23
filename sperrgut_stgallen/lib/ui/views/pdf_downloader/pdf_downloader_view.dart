9fa76316d3743b3fdaf6b83f9a95565ba2f5260a
silasETH <silstocker@student.ethz.ch>
2023-03-23 20:26:31 +0100

    FloatingActionButton

--- sperrgut_stgallen/lib/ui/views/pdf_downloader/pdf_downloader_view.dart
+++ sperrgut_stgallen/lib/ui/views/pdf_downloader/pdf_downloader_view.dart
@@ -4,7 +4,6 @@ import 'package:stacked/stacked.dart';
 
 import '../../common/app_colors.dart';
 import '../../common/custom_appbar.dart';
-import '../../common/ui_helpers.dart';
 import 'pdf_downloader_viewmodel.dart';
 
 class PdfDownloaderView extends StackedView<PdfDownloaderViewModel> {
@@ -21,46 +20,16 @@ class PdfDownloaderView extends StackedView<PdfDownloaderViewModel> {
         back: viewModel.pop,
         home: viewModel.home,
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(mediumSize),
-              child: Text(
-                "PDF Download",
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-            ),
-            Center(
-              child: MaterialButton(
-                padding: const EdgeInsets.all(20),
-                color: Colors.white,
-                onPressed: () async => openFile(await viewModel.makePdf()),
-                child: SizedBox(
-                  height: 120,
-                  width: 300,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: const [
-                      verticalSpaceSmall,
-                      Icon(
-                        Icons.picture_as_pdf_outlined,
-                        color: kcDarkGreyColor,
-                        size: 50,
-                      ),
-                      verticalSpaceSmall,
-                      Text(
-                        'PDF herunterladen',
-                        style: TextStyle(color: kcDarkGreyColor, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+      floatingActionButton: SizedBox(
+        width: 80,
+        height: 80,
+        child: FloatingActionButton(
+          onPressed: () async => openFile(await viewModel.makePdf()),
+          child: const Icon(
+            Icons.qr_code,
+            size: 40,
+          ),
+          backgroundColor: kcPrimaryColor,
         ),
       ),
     );
